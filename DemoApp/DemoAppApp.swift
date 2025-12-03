015c88e61930d142b31fd0ccd337ad036e0c6a4b
developer-yagnik <yagnikpatel5253@gmail.com>
2025-12-03 14:43:29 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Yagnikk on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
